fef7aab07f6ed360541b117399f33f8501a5c7d9
Liam Davis <114543370+LiamDavisSAE@users.noreply.github.com>
2022-11-23 12:57:34 +1100

    Initial commit

---
+++ Assets/Scripts/SpikyBrambles.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LiamDavis
+{
+   public class SpikyBrambles : MonoBehaviour
+    {
+        public void OnCollisionEnter(Collision collision)
+        {
+            
+        }
+    }        
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# 22T3_GAD170.3_Davis_Liam
+ Project 3 Game Scripting
